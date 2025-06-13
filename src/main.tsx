7bb8d4c79c63869e8aebb8f9d34673945bb6411c
alexis <alexisrk310@gmail.com>
2025-06-13 00:57:30 -0500

    arreglando

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,14 @@
-import { StrictMode } from 'react';
+import { StrictMode, useEffect } from 'react';
 import { createRoot } from 'react-dom/client';
+import { initMercadoPago } from '@mercadopago/sdk-react';
 import './index.css';
 import App from './App';
 
+// Reemplaza esto por tu clave real o usa una variable de entorno si configuras dotenv.
+initMercadoPago("APP_USR-8e757599-164c-4d80-9888-bad2ee1e5881");
+
 createRoot(document.getElementById('root')!).render(
-	<StrictMode>
-		<App />
-	</StrictMode>
+  <StrictMode>
+    <App />
+  </StrictMode>
 );
